d33228ddc14839a5394abe3d3f8f2a668caaeee8
hackeris <hackeris@qq.com>
2014-12-03 14:09:19 +0800

    add getAllBoughtCount

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,4 +1,23 @@
 //TODO: Please write code in this file.
 function printInventory(inputItems){
+
+	var boughtCount = getAllBoughtCount(inputItems);
+}
+
+function getAllBoughtCount(inputItems){
 	
+	var allCounts = new Array();
+	for (var i = 0;i < inputItems.length; i++){
+		var barcode = inputItems[i].split('-')[0];
+		var count = inputItems[i].split('-')[1];
+		if(!allCounts[barcode]){
+			allCounts[barcode] = 0;
+		}
+		if(count){
+			allCounts[barcode] += parseInt(count);
+			continue;
+		}
+		allCounts[barcode] ++;
+	}
+	return allCounts;
 }

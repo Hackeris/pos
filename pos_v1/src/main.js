88798494565eb0fa4d3e7c147f333b7da263eba9
hackeris <hackeris@qq.com>
2014-12-07 01:47:33 +0800

    function for generating gifts receipt

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -8,6 +8,11 @@ function build_paying_ticket(paying_list){
 		},"");
 }
 
+function build_gifts_receipt(gifts_list){
+	return _.reduce(gifts_list,function(str,it){
+		return str + "名称：" + it.name + "，数量：" + it.count + it.unit + "\n";
+	},"");
+}
 
 function printInventory(inputItems){
 
@@ -35,12 +40,28 @@ function printInventory(inputItems){
 		}
 	];
 
+	var gifts_list = [
+		{
+			name : "雪碧",
+			price : "3.00",
+			count : "1",
+			costs : "3.00",
+			unit : "瓶",
+		},
+		{
+			name : "方便面",
+			price : "4.50",
+			count : "1",
+			costs : "4.50",
+			unit : "袋",
+		}
+	];
+
 	var text = "***<没钱赚商店>购物清单***\n"+
 		build_paying_ticket(paying_list)+
 		"----------------------\n"+
 		"挥泪赠送商品：\n"+
-		"名称：雪碧，数量：1瓶\n"+
-		"名称：方便面，数量：1袋\n"+
+		build_gifts_receipt(gifts_list) +
 		"----------------------\n"+
 		"总计：51.00(元)\n"+
 		"节省：7.50(元)\n"+

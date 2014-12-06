2075f4954c2773d9210a95fdafcdc446a2916b29
hackeris <hackeris@qq.com>
2014-12-07 01:33:52 +0800

    get paid_ticket out and use _.reduce to build message

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -2,11 +2,40 @@
 
 function printInventory(inputItems){
 
-	var text = "***<没钱赚商店>购物清单***\n"+
-		"名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n"+
-		"名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n"+
-		"名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n"+
-		"----------------------\n"+
+	var paid_ticket = [
+		{
+			name : "雪碧",
+			price : "3.00",
+			count : "5",
+			paid : "12.00",
+			unit : "瓶",
+		},
+		{
+			name : "荔枝",
+			price : "15.00",
+			count : "2",
+			paid : "30.00",
+			unit : "斤",
+		},
+		{
+			name : "方便面",
+			price : "4.50",
+			count : "3",
+			paid : "9.00",
+			unit : "袋",
+		}
+	];
+
+	var text = "***<没钱赚商店>购物清单***\n";
+		
+		var paid_list = _.reduce(paid_ticket,function(str,it){
+			return str + "名称："+ it.name + "，数量："+it.count + it.unit + 
+			"，单价：" + it.price + "(元)，小计："+ it.paid +"(元)\n";
+		},"");
+
+		text += paid_list;
+
+		text += "----------------------\n"+
 		"挥泪赠送商品：\n"+
 		"名称：雪碧，数量：1瓶\n"+
 		"名称：方便面，数量：1袋\n"+
@@ -17,3 +46,4 @@ function printInventory(inputItems){
 
 	console.log(text);
 }
+

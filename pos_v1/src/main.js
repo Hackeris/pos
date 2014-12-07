73b1b31f61c2f72c7c610e038d5565cc5afc57c5
hackeris <hackeris@qq.com>
2014-12-07 22:22:22 +0800

    build receipt

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -17,69 +17,70 @@ function build_gifts_receipt(gifts_list){
 
 function build_total_label(paying_list){
 
-	return "总计：" + _.reduce(paying_list,function(mamo,it){
-		return mamo + parseFloat(it.costs);
+	return "总计：" + _.reduce(paying_list,function(memo,it){
+		return memo + parseFloat(it.costs);
 	},0.0).toFixed(2) + "(元)\n";
 }
 
 function build_saved_label(gifts_list){
 
-	return "节省：" + _.reduce(gifts_list,function(mamo,it){
-		return mamo + parseFloat(it.costs);
+	return "节省：" + _.reduce(gifts_list,function(memo,it){
+		return memo + parseFloat(it.costs);
 	},0).toFixed(2) + "(元)\n";
 }
 
-function printInventory(inputItems){
+function printInventory(inputItems) {
 
-	var paying_list = [
-		{
-			name : "雪碧",
-			price : "3.00",
-			count : "5",
-			costs : "12.00",
-			unit : "瓶",
-		},
-		{
-			name : "荔枝",
-			price : "15.00",
-			count : "2",
-			costs : "30.00",
-			unit : "斤",
-		},
-		{
-			name : "方便面",
-			price : "4.50",
-			count : "3",
-			costs : "9.00",
-			unit : "袋",
-		}
-	];
-
-	var gifts_list = [
-		{
-			name : "雪碧",
-			price : "3.00",
-			count : "1",
-			costs : "3.00",
-			unit : "瓶",
-		},
-		{
-			name : "方便面",
-			price : "4.50",
-			count : "1",
-			costs : "4.50",
-			unit : "袋",
-		}
-	];
+	var receipt = {
+		paying_items : [
+			{
+				name : "雪碧",
+				price : "3.00",
+				count : "5",
+				costs : "12.00",
+				unit : "瓶"
+			},
+			{
+				name : "荔枝",
+				price : "15.00",
+				count : "2",
+				costs : "30.00",
+				unit : "斤"
+			},
+			{
+				name : "方便面",
+				price : "4.50",
+				count : "3",
+				costs : "9.00",
+				unit : "袋"
+			}
+		],
+		gifts_list : [
+			{
+				name : "雪碧",
+				price : "3.00",
+				count : "1",
+				costs : "3.00",
+				unit : "瓶"
+			},
+			{
+				name : "方便面",
+				price : "4.50",
+				count : "1",
+				costs : "4.50",
+				unit : "袋"
+			}
+		]
+	};
 
 	var text = "***<没钱赚商店>购物清单***\n"+
-		build_paying_receipt(paying_list)+
+		build_paying_receipt(receipt.paying_items)+
 		"----------------------\n"+
 		"挥泪赠送商品：\n"+
-		build_gifts_receipt(gifts_list) +
+		build_gifts_receipt(receipt.gifts_list) +
 		"----------------------\n"+
-		build_total_label(paying_list)+
-		build_saved_label(gifts_list)+
+		build_total_label(receipt.paying_items)+
+		build_saved_label(receipt.gifts_list)+
 		"**********************";
 
 	console.log(text);
